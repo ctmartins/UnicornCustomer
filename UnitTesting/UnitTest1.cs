2059233eeac68df6fc06cf1d01efc3ffbceabef3
unknown <michael_fisher2@hotmail.co.uk>
2018-03-05 14:51:36 +0000

    first commit

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDatabaseIsValid()
+        {
+            var db = new UnicornCustomer.Database.Customer();
+
+           // assert
+            
+
+        }
+    }
+}
