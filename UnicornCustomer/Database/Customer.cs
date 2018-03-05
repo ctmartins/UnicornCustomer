9eed981c802a423aada035f8e07dace2ac95d4af
AnthonyArdron <36671914+AnthonyArdron@users.noreply.github.com>
2018-03-05 15:29:58 +0000

    Added Database access

--- UnicornCustomer/Database/Customer.cs
+++ UnicornCustomer/Database/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,29 @@ namespace UnicornCustomer.Database
     public class Customer
     {
 
-        public void Save()
-        {
 
+            public bool Save(string FirstName, string LastName, string Nationality, int Age)
+            {
+                var connstring = @"Server = .\SQLEXPRESS; Initial Catalog = Test;UserName = sa;password = admin";
+
+                using (SqlConnection openCon = new SqlConnection(connstring))
+                {
+                    var ssql = "insert into dbo.Customer"
+                        + " ([First Name], [Last Name], [Nationality], [Age])"
+                        + " values (@FirstName, @LastName, @Nationality, @Age)";
+
+                    using (SqlCommand sqlCommand = new SqlCommand(ssql))
+                    {
+                        sqlCommand.Parameters.AddWithValue("@FirstName", FirstName);
+                        sqlCommand.Parameters.AddWithValue("@LastName", LastName);
+                        sqlCommand.Parameters.AddWithValue("@Nationality", Nationality);
+                        sqlCommand.Parameters.AddWithValue("@Age", Age);
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+
+
+                return false;
         }
     }
 }
