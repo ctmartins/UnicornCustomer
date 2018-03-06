fc6fb5d099d4f3db70ff3cb6b95ebcef23d9156a
unknown <michael_fisher2@hotmail.co.uk>
2018-03-06 09:13:25 +0000

    web changes

--- UnicornCustomer/App_Start/WebApiConfig.cs
+++ UnicornCustomer/App_Start/WebApiConfig.cs
@@ -16,8 +16,13 @@ namespace UnicornCustomer
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+            routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "Api",
+            routeTemplate: "api/{controller}"
             );
         }
     }
